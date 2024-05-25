78f9b6cc15e978b25447c97a0a79d4905612c095
Davi <149123949+Davi-Souza-Dev@users.noreply.github.com>
2024-05-25 09:24:35 -0300

    Mobile

---
+++ srcipt/funcoes.js
@@ -0,0 +1,8 @@
+// Menu Mobile
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
