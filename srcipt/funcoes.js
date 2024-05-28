804f03671fc38b1f413ee9abe7cb7d02c180e471
Davi <149123949+Davi-Souza-Dev@users.noreply.github.com>
2024-05-28 19:00:02 -0300

    Página anuncios pronta

--- srcipt/funcoes.js
+++ srcipt/funcoes.js
@@ -1,8 +1,26 @@
 // Menu Mobile
 function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// <!-- Initialize Swiper -->
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
